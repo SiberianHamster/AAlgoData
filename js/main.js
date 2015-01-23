9609673784de1b8451c8cb2ae86e1fc82fa83a79
Mark Lin <m4rklin@comcast.net>
2015-01-23 01:40:16 -0800

    Add comment on code structure

--- js/main.js
+++ js/main.js
@@ -6,11 +6,26 @@ $(".clickme").click(function(){
 function increaseNumbertoSort(){
   var newNumbertoAdd = ($(".liList").length)+1;
   $(".sortlist").append("<li class='liList'>" + newNumbertoAdd + "</li>");
+  //add new chlid row to table
 }
 
 function  bogoSortThis(){
-    // Randomnizer() random array
-   // Checknizer() check array for sortness}
+
+  // loop the entire function until we set Done to True.
+  // keep count of how many times we did this and replace to last child row of table
+
+
+    var maxLiCount = ($(".liList").length);
+    // loop this section until maxLiCount=0
+        // random from 0 to maxLiCount
+        // eq that random to the end
+        // swap the end -1 to where random was
+        //decriment maxlicount by 1
+
+    //loop the check while i < maxLicount or i=li's i
+        // each,  eq of i of li = i until li = maxLiCount
+        // anytime if i>maxLiCount set done to true
+
 }
 
 

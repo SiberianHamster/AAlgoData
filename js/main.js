3ff4caee158020d6705609cd29f844b1060e3ae2
Mark Lin <m4rklin@comcast.net>
2015-01-24 17:25:03 -0800

    Fix randomnizer

--- js/main.js
+++ js/main.js
@@ -4,29 +4,45 @@ $(".clickme").click(function(){
 });
 
 function increaseNumbertoSort(){
-  var newNumbertoAdd = ($(".liList").length)+1;
+  var newNumbertoAdd = ($(".liList").length);
   $(".sortlist").append("<li class='liList'>" + newNumbertoAdd + "</li>");
   //add new chlid row to table
 }
 
 function  bogoSortThis(){
 
-  // loop the entire function until we set Done to True.
-  // keep count of how many times we did this and replace to last child row of table
+var maxLiCount = ($(".liList").length);
+for (i=maxLiCount; i>=0; i--){
+        console.log("i= " + i);
+        console.log("rand= " + rand);
+
+        var rand = Math.floor(Math.random() * (i)+1);
+        $(".liList:eq("+i+")").after($(".liList:eq("+rand+")"))
+
+}
 
 
-    var maxLiCount = ($(".liList").length);
-    // loop this section until maxLiCount=0
-        // random from 0 to maxLiCount
+
+
+
+
+
+
+
+}
+
+
+
+
+
         // eq that random to the end
         // swap the end -1 to where random was
-        //decriment maxlicount by 1
+        //decriment maxLiCount by 1
 
-    //loop the check while i < maxLicount or i=li's i
+    //loop the check while i < maxLiCount or i=li's i
         // each,  eq of i of li = i until li = maxLiCount
         // anytime if i>maxLiCount set done to true
 
-}
 
 
 

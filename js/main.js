87fadad2b4932f188b3758c7ef229c0d229f4613
Mark Lin <m4rklin@comcast.net>
2015-01-25 09:59:01 -0800

    Fix WS

--- js/main.js
+++ js/main.js
@@ -1,41 +1,37 @@
-$(".clickme").click(function(){
-  increaseNumbertoSort();
-  bogoSortThis();
-});
+  $(".clickme").click(function(){
+    increaseNumbertoSort();
+    bogoSortThis();
+  });
 
-function increaseNumbertoSort(){
-  var newNumbertoAdd = ($(".liList").length)+1;
-  $(".sortlist").append("<li class='liList'>" + newNumbertoAdd + "</li>");
-}
+  function increaseNumbertoSort(){
+    var newNumbertoAdd = ($(".liList").length)+1;
+    $(".sortlist").append("<li class='liList'>" + newNumbertoAdd + "</li>");
+  }
 
-function  bogoSortThis(){
-  var itIsSorted = false;
-  var howManyTimesTried = 0;
-  var maxLiCount = ($(".liList").length);
+  function  bogoSortThis(){
+    var itIsSorted = false;
+    var howManyTimesTried = 0;
+    var maxLiCount = ($(".liList").length);
 
-  while (itIsSorted == false) {
-    howManyTimesTried++;
-    console.log("how many times " + howManyTimesTried)
+    while (itIsSorted == false) {
+      howManyTimesTried++;
 
-    for (var i=maxLiCount; i>=0; i--){
-      var rand = Math.floor(Math.random() * (i)+1);
-      $(".liList:nth-child("+i+")").after($(".liList:nth-child("+rand+")"));
-    } console.log("done randomizing")
 
-    var i = 1;
-
-    while (i<=maxLiCount){
-      console.log("i "+i);
-      console.log("maxLiCount "+maxLiCount);
-      if (($(".liList:nth-child("+i+")").html() == i)){
-        i++;
-        itIsSorted=true;
-        console.log("true")
+      for (var i=maxLiCount; i>=0; i--){
+        var rand = Math.floor(Math.random() * (i)+1);
+        $(".liList:nth-child("+i+")").after($(".liList:nth-child("+rand+")"));
       }
-      else{itIsSorted=false
-        i=maxLiCount+1;
-        console.log("false")
+
+      var i = 1;
+
+      while (i<=maxLiCount){
+        if (($(".liList:nth-child("+i+")").html() == i)){
+          i++;
+          itIsSorted=true;
+        }
+        else{itIsSorted=false
+          i=maxLiCount+1;
+        }
       }
     }
   }
-}

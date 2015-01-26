8f918fc898cc7724b353118aa81a5bc63b0b27ff
Mark Lin <m4rklin@comcast.net>
2015-01-26 00:16:11 -0800

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
   $(".clickme").click(function(){
     increaseNumbertoSort();
     bogoSortThis();
@@ -15,31 +16,32 @@
 
     while (itIsSorted == false) {
       howManyTimesTried++;
-
-    for (var i=maxLiCount; i>=0; i--){
-      var rand = Math.floor(Math.random() * (i)+1);
-      $(".liList:nth-child("+i+")").after($(".liList:nth-child("+rand+")"));
-    }
-
+      for (var i=maxLiCount; i>=0; i--){
+        var rand = Math.floor(Math.random() * (i)+1);
+        $(".liList:nth-child("+i+")").after($(".liList:nth-child("+rand+")"));
+      }
       var i = 1;
       while (i<=maxLiCount){
         if (($(".liList:nth-child("+i+")").html() == i)){
           i++;
           itIsSorted=true;
         }
-        else{itIsSorted=false
+        else{
+          itIsSorted=false
           i=maxLiCount+1;
         }
       }
     }
     var x = maxLiCount;
     var AverageScenario = 1;
-    while(x>0){AverageScenario=AverageScenario*x;x--;}
+    while(x>0){
+      AverageScenario=AverageScenario*x;x--;
+    }
     $(".resulttable").append("<tr class='tablerow'><td>" + maxLiCount + "</td><td>" + howManyTimesTried + "</td><td>" + AverageScenario + "</td></tr>");
     if (howManyTimesTried==1) {
       $(".imageholder").replaceWith('<img class="imageholder" src="img/2.jpg">')
     }
-      else {
-        $(".imageholder").replaceWith('<img class="imageholder" src="img/1.jpg">')
-      }
+    else {
+      $(".imageholder").replaceWith('<img class="imageholder" src="img/1.jpg">')
+    }
   }

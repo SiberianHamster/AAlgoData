8a07bb29c756013e73c11f024e0cc2d3307955ac
Mark Lin <m4rklin@comcast.net>
2015-02-11 13:24:13 -0800

    Fix doulbe printout

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 $(function(){
 
-  var isItSorted = false;
   var howManyTimesTried = 0;
   var currentListCount;
-  var i=currentListCount;
+  var i = currentListCount;
 
   function increaseArrayForSorting(){
     var nextNumberToAddToDOM = ($(".liList").length) + 1;
@@ -13,38 +12,36 @@ $(function(){
 
   function checkMeForSortness(){
     var i = 1;
-    while (i<=(($(".liList").length))){
+    while (i <= (($(".liList").length))){
       if (($(".liList:nth-child("+i+")").html() == i)){
         i++;
-        if(i >= $(".liList").length){
-          console.log("Number of Tries " + howManyTimesTried)
+        if(i > $(".liList").length){
+          var x = $(".liList").length;
+          var AverageScenario = 1;
 
-    // var x = currentListCount;
-    // var AverageScenario = 1;
-    // while(x>0){
-    //   AverageScenario = AverageScenario * x;
-    //   x--;
-    // }
-    // $(".resulttable").append("<tr class='tablerow'><td>" + currentListCount + "</td><td>" + howManyTimesTried + "</td><td>" + AverageScenario + "</td></tr>");
-    // if (howManyTimesTried==1){
-    //   $(".imageholder").replaceWith('<img class="imageholder" src="img/2.jpg">')
-    // }
-    // else {
-    //   $(".imageholder").replaceWith('<img class="imageholder" src="img/1.jpg">')
-    // }
+          while(x > 0){
+            AverageScenario = AverageScenario * x;
+            x--;
+          }
 
+          $(".resulttable").append("<tr class='tablerow'><td>" + $(".liList").length + "</td><td>" + howManyTimesTried + "</td><td>" + AverageScenario + "</td></tr>");
 
+          if (howManyTimesTried==1){
+            $(".imageholder").replaceWith('<img class="imageholder" src="img/2.jpg">')
+          }
 
+          else {
+           $(".imageholder").replaceWith('<img class="imageholder" src="img/1.jpg">')
+          }
         }
+        $(".clickme").fadeIn();
       }
       else{
-        isItSorted=false
-        i=currentListCount + 1;
+        i = $(".liList").length + 1;
         howManyTimesTried++;
         setTimeout(randomMe, 1000, i);
       }
     }
-
   }
 
   var randomMe = function (i){
@@ -59,23 +56,9 @@ $(function(){
     }
   }
 
-      // var x = currentListCount;
-    // var AverageScenario = 1;
-    // while(x>0){
-    //   AverageScenario = AverageScenario * x;
-    //   x--;
-    // }
-  //   $(".resulttable").append("<tr class='tablerow'><td>" + currentListCount + "</td><td>" + howManyTimesTried + "</td><td>" + AverageScenario + "</td></tr>");
-  //   if (howManyTimesTried==1) {
-  //     $(".imageholder").replaceWith('<img class="imageholder" src="img/2.jpg">')
-  //   }
-  //   else {
-  //     $(".imageholder").replaceWith('<img class="imageholder" src="img/1.jpg">')
-  //   }
-  // }
-
   $(".clickme").click(function(){
-    howManyTimesTried = 0;
+    $(".clickme").fadeOut();
+    howManyTimesTried = 1;
     increaseArrayForSorting();
     randomMe(currentListCount);
   })
